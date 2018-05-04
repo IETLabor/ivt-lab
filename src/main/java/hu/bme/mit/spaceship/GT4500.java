da68d0a756a471469cd412265d8f9fafda3b6866
IETLabor <gustorn+ietlabor@gmail.com>
2018-05-04 01:29:42 -0700

    Remove redundant assignment to firingSuccess

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,11 +79,11 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         if (!primaryTorpedoStore.isEmpty()) {
-          firingSuccess = primaryTorpedoStore.fire(1);
+          primaryTorpedoStore.fire(1);
           firingSuccess = true;
         }
         if (!secondaryTorpedoStore.isEmpty()) {
-          firingSuccess = secondaryTorpedoStore.fire(1);
+          secondaryTorpedoStore.fire(1);
           firingSuccess = true;
         }
 
